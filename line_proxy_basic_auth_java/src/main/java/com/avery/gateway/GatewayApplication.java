ed950700ad72aa0d5e305512e3d983f6df41644b
Zhengyu Zhu <Zhengyu@Zhengyus-MacBook-Pro.local>
2017-11-20 22:13:19 +0800

    add web.xml

--- line_proxy_basic_auth_java/src/main/java/com/avery/gateway/GatewayApplication.java
+++ line_proxy_basic_auth_java/src/main/java/com/avery/gateway/GatewayApplication.java
@@ -2,13 +2,20 @@ package com.avery.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import com.avery.gateway.filters.pre.*;
 
 @EnableZuulProxy
 @SpringBootApplication
-public class GatewayApplication {
+public class GatewayApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GatewayApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
